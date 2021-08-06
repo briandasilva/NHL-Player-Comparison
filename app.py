56639c610e115363fd94693324b9fa80008622d2
Brian da Silva <dasilva.bri@northeastern.edu>
2021-08-06 15:39:12 -0400

    Initial Data Table and Updated Bulma

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template
 from bs4 import BeautifulSoup
 import requests
+import lxml
+import pandas as pd
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    source = requests.get("https://www.hockey-reference.com/leagues/NHL_2021_skaters.html").text
+    soup = BeautifulSoup(source, "lxml")
+
+    result = pd.DataFrame()
+    url = "https://www.hockey-reference.com/leagues/NHL_2021_skaters.html"
+    df = pd.read_html(url, header=1)[0]
+    result = result.append(df, sort=False)
+
+    result = result[~result["Age"].str.contains("Age")]
+    result = result.reset_index(drop=True)
+
+    table = pd.DataFrame.to_html(result)
+
+    return render_template("index.html", tables=[result.to_html(classes="data")], titles=result.columns.values)
